67453085c97a3ebbdc0c15a6a0b0306eacf76c00
Ryan Williams <ryan.williams@projectpeople.com>
2017-11-07 15:52:10 +0000

    Completed Lesson 155

--- src/main/java/guru/springframework/recipeproject/recipes/controllers/RecipeController.java
+++ src/main/java/guru/springframework/recipeproject/recipes/controllers/RecipeController.java
@@ -57,11 +57,12 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(){
+    public ModelAndView handleNotFound(Exception exception){
         log.error("Handling not found exception.");
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("errors/404");
+        modelAndView.addObject("exception", exception);
 
         return modelAndView;
     }
